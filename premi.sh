6352653999a798204e45ead41deff69da7cfcb42
Peyxmrz <154864166+Mrz051@users.noreply.github.com>
2024-08-15 23:13:47 +0700

    Add files via upload

--- premi.sh
+++ premi.sh
@@ -266,10 +266,10 @@ TEXT="
 💥 » <code>Owner    :</code><code>$username</code>
 💥 » <code>Domain   :</code><code>$domain</code>
 💥 » <code>IPVPS    :</code><code>$IP</code>
-💥 » <code>ISP      :</code><code>${ISP{</code>
-💥 » <code>OS       :</code><code>${OS}</code>
-💥 » <code>COUNTRY  :</code><code>{CITY}</code>
-💥 » <code>EXP SC   :</code><code>{expe} Hari</code>
+💥 » <code>ISP      :</code><code>$ISP</code>
+💥 » <code>OS       :</code><code>$OS</code>
+💥 » <code>COUNTRY  :</code><code>$CITY</code>
+💥 » <code>EXP SC   :</code><code>$expe Hari</code>
 <code>────────────────────</code>
 <b> SCRIPT NOTIF </b>
 <code>────────────────────</code>
